94fe7ba4cf491ba6f1916e3ac3c1c45e20bd4ac1
Shivam Bohra <shivambohracse@gmail.com>
2025-02-02 22:23:36 +0530

    added

---
+++ Code/cf/1002/A.cpp
@@ -0,0 +1,72 @@
+#ifndef ONLINE_JUDGE
+#include "magic.h"
+#else
+#include<bits/stdc++.h>
+#endif
+#include <set>
+using namespace std;
+typedef long long ll;
+#define mod 1000000007
+vector<bool> prime;
+ 
+void sieve(ll n)
+{
+	prime.resize(n + 1, true);
+	prime[0] = prime[1] = false;
+	for (ll i = 2; i <= n; i++) {
+		if (prime[i] && i * i <= n) {
+			for (ll j = i * i; j <= n; j += i)
+				prime[j] = false;
+		}
+	}
+}
+ 
+template<typename T>
+std::istream& operator>>(std::istream&in, std::vector<T>& v) {
+	for (T& x : v)
+		in >> x;
+	return in;
+}
+ 
+template<typename T>
+std::ostream& operator<<(std::ostream&out, std::vector<T>& v) {
+	for (const T& x : v)
+		std::cout << x << ' ';
+	std::cout << '\n';
+	return out;
+}
+ 
+void solve()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n), b(n);
+	cin >> a >> b;
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	set<int> x, y;
+	for (int i = 0; i < n; i++) {
+		x.insert(a[i]);
+		y.insert(b[i]);
+	}
+	if (max(x.size(), y.size()) > 2 || (x.size() + y.size()) > 3) {
+		cout << "Yes";
+	} else {
+		cout << "No";
+	}
+}
+ 
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+ 
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+	return 0;
+}
