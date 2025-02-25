190747e67543ac114e52e682ca1fc20d685521ea
Shivam Bohra <shivambohracse@gmail.com>
2025-02-25 19:23:49 +0530

    added

---
+++ Code/Resources/templates/disjoing_set.cpp
@@ -0,0 +1,12 @@
+DISJOINT SET
+
+Given two disjoint sets (which do not have any element in common) we can merge them and find the union and representative of the set.
+
+This will also help us in finding whether two elements belong to the same set or not. (Generally using their set representatives (aka leaders))
+
+DSU helps determine whether two nodes belong to the same component.
+
+Used in offline dynamic connectivity problems, where we preprocess queries to check connectivity between nodes.
+
+
+
