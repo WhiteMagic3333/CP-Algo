bd6884a9a5d92fccf7f3724838670d6ac3078291
Shivam Bohra <shivambohracse@gmail.com>
2025-04-02 11:49:39 +0530

    added

---
+++ Code/Leetcode/Daily/Apr_2025/2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long ans = 0, n = nums.size(), left = 0, pre_max = 0;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, left * nums[i]);
+            left = max(left, pre_max - nums[i]);
+            pre_max = max(pre_max, (long long)nums[i]);
+        }
+        return ans;
+    }
+};
