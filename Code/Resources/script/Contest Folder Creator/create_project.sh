3d8359e756016e0ffb7200298d3ef4ff6cc6846e
whitemagic3333 <whitemagic7456@gmail.com>
2025-11-06 13:26:43 +0530

    added

--- Code/Resources/script/Contest Folder Creator/create_project.sh
+++ Code/Resources/script/Contest Folder Creator/create_project.sh
@@ -31,5 +31,9 @@ g++ -std=c++11 magic.h || { echo "Failed to precompile magic.h"; exit 1; }
 # Notify user
 echo "Folder '$folder_name' created with files A.cpp, B.cpp, C.cpp, D.cpp, E.cpp, inputf.in, outputf.out, and precompiled magic.h."
 
-# Open the files in Sublime Text and set the layout
-subl --command "focus_group {\"group\": 1}" && subl inputf.in && subl --command "focus_group {\"group\": 2}" && subl outputf.out && subl --command "focus_group {\"group\": 0}" && subl E.cpp && subl E.cpp && subl D.cpp && subl C.cpp && subl B.cpp && subl A.cpp 
+# Close all files, set layout, and open files in specific groups
+subl --command "close_all" && \
+subl --command "set_layout {\"cols\": [0.0, 0.5, 1.0], \"rows\": [0.0, 0.5, 1.0], \"cells\": [[0, 0, 1, 2], [1, 0, 2, 1], [1, 1, 2, 2]]}" && \
+subl --command "focus_group {\"group\": 1}" && subl inputf.in && \
+subl --command "focus_group {\"group\": 2}" && subl outputf.out && \
+subl --command "focus_group {\"group\": 0}" && subl E.cpp && subl D.cpp && subl C.cpp && subl B.cpp && subl A.cpp
